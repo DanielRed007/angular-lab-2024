9697a7727fc80bc1694af0f35102b65a00fe3909
danielred007 <danielrojo1927@gmail.com>
2024-01-31 22:47:53 +0000

    v2

--- lab-1/src/app/components/form-array/form-array.component.ts
+++ lab-1/src/app/components/form-array/form-array.component.ts
@@ -9,35 +9,20 @@ import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 export class FormArrayComponent {
   title = 'FormArray Example in Angular Reactive forms';
 
-  skillsForm: FormGroup;
+  constructor(public fb: FormBuilder) {}
 
-  constructor(private fb: FormBuilder) {
-    this.skillsForm = this.fb.group({
-      name: '',
-      skills: this.fb.array([]),
-    });
+  registrationForm = this.fb.group({
+    addDynamicElement: this.fb.array([]),
+  });
+
+  get addDynamicElement() {
+    return this.registrationForm.get('addDynamicElement') as FormArray;
   }
-
-  get skills(): FormArray {
-    return this.skillsForm.get('skills') as FormArray;
-  }
-
-  newSkill(): FormGroup {
-    return this.fb.group({
-      skill: '',
-      exp: '',
-    });
-  }
-
-  addSkills() {
-    this.skills.push(this.newSkill());
-  }
-
-  removeSkill(i: number) {
-    this.skills.removeAt(i);
+  addItems() {
+    this.addDynamicElement.push(this.fb.control(''));
   }
 
   onSubmit() {
-    console.log(this.skillsForm.value);
+    alert(JSON.stringify(this.registrationForm.value));
   }
 }
